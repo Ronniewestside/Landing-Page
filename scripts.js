17b37cb200ddc3c59ef445f865c2649ab9936728
Ronniewestside <145373325+Ronniewestside@users.noreply.github.com>
2024-08-01 19:33:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
